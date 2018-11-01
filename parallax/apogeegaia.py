9e847fca3adc07804e36fe36c3aac636e73f93cb
Andrew Jones <andyjones.ed@gmail.com>
2018-11-01 16:48:29 -0400

    Adds some logging

--- parallax/apogeegaia.py
+++ parallax/apogeegaia.py
@@ -5,10 +5,13 @@ import astropy
 import os
 from astroquery.gaia import Gaia
 from . import s3
+import logging
 
 os.environ['GAIA_TOOLS_DATA'] = '/home/ec2-user/code/data/gaia_tools'
 import gaia_tools
 import gaia_tools.load
+
+log = logging.getLogger(__name__)
 
 PATH = 'alj.data/parallax/apogee_gaia.fits'
 
@@ -36,12 +39,16 @@ def stringify(table):
         table[o] = table[o].astype(str)
 
 def _load():
+    log.info('Constructing APOGEE-GAIA crossmatch')
     apogee_cat = gaia_tools.load.apogee()
     gaia_subtable, _ = gaia_tools.xmatch.cds(apogee_cat, xcat='vizier:I/345/gaia2')
+
+    log.info('Expanding GAIA matches to the full columnset')
     gaia_table = expand_gaia(gaia_subtable)
 
     indices = gaia_tools.xmatch.cds_matchback(apogee_cat, gaia_table)
 
+    log.info('Saving crossmatch to S3')
     apogee_table = astropy.table.Table(apogee_cat[indices])
     table = astropy.table.hstack([gaia_table, apogee_table])
     stringify(table)
@@ -55,5 +62,6 @@ def _load():
 def load():
     path = s3.Path(PATH)
     if not path.exists():
+        log.info('No apogee-gaia cache available, creating it from scratch')
         path.write_bytes(_load())
     return astropy.table.Table.read(BytesIO(path.read_bytes()))
