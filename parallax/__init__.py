c14ed085e8d7fd6928584c4b999b48ecb4153db1
Andrew Jones <andyjones.ed@gmail.com>
2018-11-07 15:59:00 +0000

    Minor fixes

--- parallax/__init__.py
+++ parallax/__init__.py
@@ -13,14 +13,14 @@ def startup():
     sess = ec2.session(instance)
 
 def parent_sample(catalogue):
-    cuts = {'upper_g': catalogue['LOGG'] <= 2.2,
-            'nonnull_g': catalogue['LOGG'] > 0., # there are a few values less than zero that are not null
-            'nonnull_k': catalogue['K'] > 0,
-            'nonnull_bp_rp': sp.isfinite(catalogue['bp_rp']),
-            'nonnull_w1mpro': sp.isfinite(catalogue['w1mpro']),
-            'nonnull_w2mpro': sp.isfinite(catalogue['w2mpro']),
-            'nonvariable': catalogue['phot_variable_flag'] != 'VARAIBLE',
-            'nonduplicate': ~pd.Series(catalogue['tmass_id']).duplicated()}
+    cuts = {'upper_g': catalogue.apogee.logg <= 2.2,
+            'nonnull_g': catalogue.apogee.logg > 0., # there are a few values less than zero that are not null
+            'nonnull_k': catalogue.apogee.k > 0,
+            'nonnull_bp_rp': sp.isfinite(catalogue.gaia.bp_rp),
+            'nonnull_w1mpro': sp.isfinite(catalogue.wise.w1mpro),
+            'nonnull_w2mpro': sp.isfinite(catalogue.wise.w2mpro),
+            'nonvariable': catalogue.gaia.phot_variable_flag != 'VARAIBLE',
+            'nonduplicate': ~catalogue.tmass.tmass_id.duplicated()}
     cut = sp.all(sp.vstack(cuts.values()).data, 0)
     return catalogue[cut]
 
